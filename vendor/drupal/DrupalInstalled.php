da32b20fb2a42f330a56454efe667d8820f49f4d
Maayukh Misra <maayukhmisra@gmail.com>
2026-09-04 00:12:01 +0530

    Install Backup and Migrate module

--- vendor/drupal/DrupalInstalled.php
+++ vendor/drupal/DrupalInstalled.php
@@ -16,6 +16,6 @@ class DrupalInstalled {
   /**
    * A hash of all the installed packages and their versions.
    */
-  public const string VERSIONS_HASH = '02bc7bbb8f56272e';
+  public const string VERSIONS_HASH = '57aa271b179b6492';
 
 }
